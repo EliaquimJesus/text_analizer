e3c3f2775f12062eb5469a36d77def27d01d9e25
Eliaquim da Luz <eliaquim.daluz@yahoo.com>
2024-01-03 12:47:35 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    // State variables
+    const [Words, setWords] = useState(0);
+    const [characters, setCharacters] = useState(0);
+    const [sentences, setSentences] = useState(0);
+    const [paragraphs, setParagraphs] = useState(0);
+    const [areaText, setAreaText] = useState("");
+
+    //variables
+    let chars, word, sentence, paragraph;
+
+    // Count number of characters, words, sentences and pharagraphs
+    chars = areaText.length;
+
+    areaText === "" ? (word = 0) : (word = areaText.split(" ").length);
+
+    areaText === "" ? (sentence = 0) : (sentence = areaText.split(".").length);
+
+    areaText === ""
+        ? (paragraph = 0)
+        : (paragraph = areaText.split("\n").length);
+
+    // Calc the biggest word, and the typing average time.
+
+    function onHandleText(value) {
+        setAreaText(value);
+        setCharacters(chars);
+        setWords(word);
+        setSentences(sentence);
+        setParagraphs(paragraph);
+    }
+
+    return (
+        <div className="app">
+            <div className="header">
+                <Statistics>
+                    Words: <span>{Words}</span>
+                </Statistics>
+                <Statistics>
+                    Characters: <span>{characters}</span>
+                </Statistics>
+                <Statistics>
+                    Sentences: <span>{sentences}</span>
+                </Statistics>
+                <Statistics>
+                    Paragraphs: <span>{paragraphs}</span>
+                </Statistics>
+            </div>
+            <Areatext onTextChange={onHandleText} />
+            <div className="footer">
+                <Statistics>Average Reading Time: </Statistics>
+                <Statistics>Longest Word: </Statistics>
+            </div>
+            <p>{areaText}</p>
+        </div>
+    );
 }
 
-export default App;
+function Statistics({ children }) {
+    return <p className="stats">{children}</p>;
+}
+
+function Areatext({ onTextChange }) {
+    const [text, setText] = useState("");
+
+    onTextChange(text);
+
+    return (
+        <div>
+            <textarea
+                rows="24"
+                cols="100"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="type text here..."
+            ></textarea>
+        </div>
+    );
+}
